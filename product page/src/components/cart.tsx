25d2bbe0db787e8eb289f500ac516884f6d0431d
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-01 17:29:39 +0800

    changed name of basket component

---
+++ product page/src/components/cart.tsx
@@ -0,0 +1,41 @@
+import { useBasket } from "../context/basketContext";
+
+function Cart() {
+  const { state, dispatch } = useBasket();
+
+  function onDeleteHandler(id: number) {
+    dispatch({ type: "REMOVE_ITEM", payload: id });
+  }
+
+  //JSX conditional elements for whether the cart is empty or full.
+  const cartEmpty = <p>Your cart is empty.</p>;
+  const cartFull = state.map((item) => {
+    const total: number = item.price * item.quantity;
+
+    return (
+      <div id={item.id.toString()}>
+        <img src={item.image} />
+        <div>
+          <p>{item.name}</p>
+          <div>
+            <p>
+              {item.price} x {item.quantity}
+            </p>
+            <p className="bold">${total}</p>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            onDeleteHandler(item.id);
+          }}
+        >
+          delete
+        </button>
+      </div>
+    );
+  });
+
+  let isCartEmpty: boolean = state.length == 0;
+
+  return <div>{isCartEmpty ? cartEmpty : cartFull}</div>;
+}
