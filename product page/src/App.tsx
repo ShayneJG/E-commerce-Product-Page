4089801611494ad33f986e27d53b1c43c697a622
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-04 11:34:54 +0800

    render cart component

--- product page/src/App.tsx
+++ product page/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import AddToCart from "./components/addToCart";
+import Cart from "./components/cart";
 
 function App() {
   return (
     <>
+      <Cart />
       <AddToCart />
     </>
   );
