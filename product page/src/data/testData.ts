781bb56799d92fb85b2d7b689169029bf6280b9a
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-04 12:14:40 +0800

    Create testData.ts

---
+++ product page/src/data/testData.ts
@@ -0,0 +1,22 @@
+import { ItemInterface } from "../components/item";
+
+export const testData: ItemInterface = {
+  thumbnails: [
+    "/public/images/image-product-1-thumbnail.jpg",
+    "/public/images/image-product-2-thumbnail.jpg",
+    "/public/images/image-product-3-thumbnail.jpg",
+    "/public/images/image-product-4-thumbnail.jpg",
+  ],
+  images: [
+    "/public/images/image-product-1.jpg",
+    "/public/images/image-product-2.jpg",
+    "/public/images/image-product-3.jpg",
+    "/public/images/image-product-4.jpg",
+  ],
+  company: "Sneaker Company",
+  title: "Fall Limited Edition Sneakers",
+  description:
+    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+  price: 125.0,
+  sale: 0.5,
+};
