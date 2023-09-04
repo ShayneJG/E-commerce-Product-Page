66eadfd46b2af63645a4c79a8a43955bc7e37d66
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-04 12:11:30 +0800

    removed add to cart

--- product page/src/App.tsx
+++ product page/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import AddToCart from "./components/addToCart";
 import Cart from "./components/cart";
 
 function App() {
   return (
     <>
       <Cart />
-      <AddToCart />
     </>
   );
 }
