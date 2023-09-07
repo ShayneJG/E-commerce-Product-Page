1abe6407054ce9d96a9535ff9e10c6ecc7d1d9ac
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-07 18:37:16 +0800

    added topbar

--- product page/src/App.tsx
+++ product page/src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Cart from "./components/cart";
 import Item from "./components/item";
+import TopBar from "./components/topBar";
 import { testData } from "./data/testData";
 
 function App() {
   return (
     <>
-      <Cart />
+      <TopBar />
       <div className="m-72"></div>
       <Item item={testData} />
     </>
