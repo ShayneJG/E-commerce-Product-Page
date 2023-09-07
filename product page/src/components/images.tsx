7fb182a5e986e110991766850bb89a73a1af602d
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-07 18:37:25 +0800

    Update images.tsx

--- product page/src/components/images.tsx
+++ product page/src/components/images.tsx
@@ -1,7 +1,29 @@
 import { useState } from "react";
+import IconNext from "/images/icon-next.svg";
+interface ImagesProps {
+  images: string[];
+}
 
-export default function Images(images: string[]) {
+export default function Images({ images }: ImagesProps) {
   const [imageIndex, setImageIndex] = useState(0);
+
+  function onMinus() {
+    if (imageIndex === 0) {
+      setImageIndex(images.length - 1);
+    } else
+      setImageIndex((prevState) => {
+        return prevState - 1;
+      });
+  }
+
+  function onPlus() {
+    if (imageIndex === images.length - 1) {
+      setImageIndex(0);
+    } else
+      setImageIndex(() => {
+        return imageIndex + 1;
+      });
+  }
 
   //render row of images
   const ImageRow: JSX.Element = (
@@ -12,5 +34,25 @@ export default function Images(images: string[]) {
     </div>
   );
 
-  return <></>;
+  return (
+    <div className="flex flex-row items-center w-full relative">
+      <button
+        className="flex items-center justify-center absolute left-5 bg-white w-10 h-10 rounded-full rotate-180 shrink-0"
+        onClick={onMinus}
+      >
+        <img src={IconNext} />
+      </button>
+
+      <img className="w-full h-full" src={images[imageIndex]} />
+
+      <button
+        className="flex items-center justify-center absolute right-5 bg-white w-10 h-10 rounded-full shrink-0"
+        onClick={onPlus}
+      >
+        <img src={IconNext} />
+      </button>
+
+      {/* {TODO: conditionally render ImageRow} */}
+    </div>
+  );
 }
