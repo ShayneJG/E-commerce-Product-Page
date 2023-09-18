45c335897335ed1d2acf3e30a45394d0b3bae2d3
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-18 12:33:28 +0800

    removed public from paths

--- product page/src/data/testData.ts
+++ product page/src/data/testData.ts
@@ -2,16 +2,16 @@ import { ItemInterface } from "../components/item";
 
 export const testData: ItemInterface = {
   thumbnails: [
-    "/public/images/image-product-1-thumbnail.jpg",
-    "/public/images/image-product-2-thumbnail.jpg",
-    "/public/images/image-product-3-thumbnail.jpg",
-    "/public/images/image-product-4-thumbnail.jpg",
+    "/images/image-product-1-thumbnail.jpg",
+    "/images/image-product-2-thumbnail.jpg",
+    "/images/image-product-3-thumbnail.jpg",
+    "/images/image-product-4-thumbnail.jpg",
   ],
   images: [
-    "/public/images/image-product-1.jpg",
-    "/public/images/image-product-2.jpg",
-    "/public/images/image-product-3.jpg",
-    "/public/images/image-product-4.jpg",
+    "/images/image-product-1.jpg",
+    "/images/image-product-2.jpg",
+    "/images/image-product-3.jpg",
+    "/images/image-product-4.jpg",
   ],
   company: "Sneaker Company",
   title: "Fall Limited Edition Sneakers",
