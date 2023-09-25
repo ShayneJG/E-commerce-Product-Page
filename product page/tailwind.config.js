e0939882b76a1f13e9fb81e2d16c60d869055b4b
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-25 12:50:39 +0800

    Update tailwind.config.js

--- product page/tailwind.config.js
+++ product page/tailwind.config.js
@@ -6,7 +6,11 @@ export default {
    "/components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '0px 3px 2px rgba(0, 0, 0, 0.50)' 
+      }
+    },
   },
   plugins: [],
 }
