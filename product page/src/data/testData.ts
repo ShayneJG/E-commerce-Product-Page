ad5fff164e5c9aa6fa97b56d2abccddbe04d1c67
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-26 12:17:46 +0800

    changed price to include pre-sale price

--- product page/src/data/testData.ts
+++ product page/src/data/testData.ts
@@ -17,6 +17,6 @@ export const testData: ItemInterface = {
   title: "Fall Limited Edition Sneakers",
   description:
     "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
-  price: 125.0,
+  price: 250.0,
   sale: 0.5,
 };
