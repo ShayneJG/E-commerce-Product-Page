9884a57de420a68d9c081ef56b8adbd62dbba912
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-26 12:17:32 +0800

    css for mobile added

--- product page/src/components/item.tsx
+++ product page/src/components/item.tsx
@@ -14,18 +14,37 @@ export interface ItemInterface {
 
 export default function Item({ item }: { item: ItemInterface }) {
   const [amount, setAmount] = useState(1);
+
+  //total is the sale price OR the normal price if there is no sale
+  const total: number = item.sale ? item.price * item.sale : item.price;
   return (
-    <div className="">
-      <div id="item-description">
-        <Images images={item.images} />
-        <p>{item.company}</p>
-        <h1>{item.title}</h1>
-        <p>{item.description}</p>
-        <div id="price-row">
-          <p>
-            ${item.price} {item.sale && `${item.sale}%`}
-          </p>
-          <div></div>
+    <div id="item" className="">
+      <Images images={item.images} />
+      <div id="item-description" className="p-5 flex flex-col gap-5">
+        <p className=" text-customOrange font-bold text-xs tracking-[1.85px]">
+          {item.company.toLocaleUpperCase()}
+        </p>
+        <h1 className="font-bold text-[28px]  leading-[32px] text-customBlack">
+          {item.title}
+        </h1>
+        <p className="text-[15px] text-customGrey leading-[25px]">
+          {item.description}
+        </p>
+        <div id="price-row" className="flex justify-between items-center">
+          <span className="font-bold text-[28px] text-customBlack">
+            ${total.toFixed(2)}
+          </span>{" "}
+          {item.sale && (
+            <div id="sale-details" className="flex justify-between w-full ml-5">
+              <span className="bg-[#ffeee2] w-[51px] h-[27px] text-center text-[16px] text-customOrange font-bold rounded-md">
+                {" "}
+                ${item.sale * 100}%
+              </span>
+              <span className="line-through	text-[#b6bcc8]">{`$${item.price.toFixed(
+                2
+              )}`}</span>
+            </div>
+          )}
         </div>
       </div>
       <AddToCart amount={amount} setAmount={setAmount} item={item} />
