cd5d971b67047090c85319a2de2a33403c0fa90e
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-27 19:06:04 +0800

    cartCount display added

--- product page/src/components/topBar.tsx
+++ product page/src/components/topBar.tsx
@@ -5,15 +5,28 @@ import IconMenu from "/images/icon-menu.svg";
 import Logo from "/images/logo.svg";
 import Cart from "./cart";
 import Menu from "./menu";
+import { useBasket } from "../context/basketContext";
 export default function TopBar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [cartOpen, setCartOpen] = useState(false);
+
+  const { state } = useBasket();
+
+  const count: number = state.reduce((acc, curr) => {
+    return (acc = acc + curr.quantity);
+  }, 0);
 
   const openHandler = (open: Dispatch<React.SetStateAction<boolean>>) => {
     open((isOpen) => {
       return !isOpen;
     });
   };
+
+  const cartCount: JSX.Element = (
+    <div className="w-[19px] h-[13px] absolute -top-[4px] right-[14px] bg-customOrange rounded-lg text-center text-white z-30 text-[10px] font-bold">
+      {count}
+    </div>
+  );
 
   //TODO: render the cart and the menu here.
   return (
@@ -27,21 +40,27 @@ export default function TopBar() {
                 openHandler(setMenuOpen);
               }}
             >
-              <img src={IconMenu} className="h-4 pr-5 m-auto drop-shadow-3xl" />
+              <img
+                src={IconMenu}
+                className="h-4 pr-5  m-auto drop-shadow-3xl"
+              />
             </button>
 
             <img src={Logo} className="drop-shadow-3xl h-full" />
           </div>
           <div id="header-right" className="flex w-1/3 place-content-end">
-            <button
-              className="px-5"
-              id="shopping-cart-icon"
-              onClick={() => {
-                openHandler(setCartOpen);
-              }}
-            >
-              <img className="drop-shadow-3xl" src={IconCart} />
-            </button>
+            <div className="relative flex justify-center">
+              {count ? cartCount : null}
+              <button
+                className="px-5"
+                id="shopping-cart-icon"
+                onClick={() => {
+                  openHandler(setCartOpen);
+                }}
+              >
+                <img className="drop-shadow-3xl" src={IconCart} />
+              </button>
+            </div>
             <div id="avatar">
               <img src={IconAvatar} className="w-6 h-6 drop-shadow-3xl" />
             </div>
