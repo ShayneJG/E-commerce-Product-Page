54ebc6f8b52bac3a14a8e5f91a10cacd53e01f08
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-27 13:31:57 +0800

    styles added for full cart

--- product page/src/components/cart.tsx
+++ product page/src/components/cart.tsx
@@ -1,5 +1,5 @@
 import { useBasket } from "../context/basketContext";
-
+import IconDelete from "/images/icon-delete.svg";
 export default function Cart() {
   const { state, dispatch } = useBasket();
 
@@ -17,15 +17,21 @@ export default function Cart() {
     const total: number = item.price * item.quantity;
 
     return (
-      <div id={item.name} key={item.name}>
-        <img src={item.image} />
-        <div>
-          <p>{item.name}</p>
-          <div>
+      <div
+        className="w-full flex justify-between"
+        id={item.name}
+        key={item.name}
+      >
+        <img className="h-12 w-12 rounded-md" src={item.image} />
+        <div className=" text-base text-customGrey">
+          <p className="">{item.name}</p>
+          <div className="flex">
             <p>
-              {item.price} x {item.quantity}
+              ${item.price.toFixed(2)} x {item.quantity}
             </p>
-            <p className="bold">${total}</p>
+            <p className="font-bold text-customBlack pl-2">
+              ${total.toFixed(2)}
+            </p>
           </div>
         </div>
         <button
@@ -33,7 +39,7 @@ export default function Cart() {
             onDeleteHandler(item.name);
           }}
         >
-          delete
+          <img src={IconDelete} />
         </button>
       </div>
     );
@@ -42,12 +48,21 @@ export default function Cart() {
   let isCartEmpty: boolean = state.length == 0;
 
   return (
-    <div className="relative w-full rounded-md bg-white z-50 h-[360px]">
-      <div className="flex flex-col w-full h-full">
+    <div className="relative w-full rounded-md bg-white z-50 h-[256px]">
+      <div className="flex flex-col w-full h-full ">
         <div className="border-b p-5 w-full">
-          <h1 className="">Cart</h1>
+          <h1 className="font-bold">Cart</h1>
         </div>
-        {isCartEmpty ? cartEmpty : cartFull}
+        <div className="p-5 overflow-auto">
+          {isCartEmpty ? cartEmpty : cartFull}
+        </div>
+        {cartFull && (
+          <div className="w-full">
+            <button className="rounded-lg w-[90%] text-center h-[56px] bg-customOrange text-white font-bold m-5">
+              Checkout
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
