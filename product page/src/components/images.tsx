84b5168f8129e5fefe04799a954d55bca8a7cf2a
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-28 22:11:14 +0800

    Update images.tsx

--- product page/src/components/images.tsx
+++ product page/src/components/images.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import IconNext from "/images/icon-next.svg";
+import ArrowButton from "./ArrowButton";
 interface ImagesProps {
   images: string[];
+  isMobile: boolean;
+  thumbnails: string[];
 }
 
-export default function Images({ images }: ImagesProps) {
+export default function Images({ images, isMobile, thumbnails }: ImagesProps) {
   const [imageIndex, setImageIndex] = useState(0);
 
   function onMinus() {
@@ -25,34 +28,60 @@ export default function Images({ images }: ImagesProps) {
       });
   }
 
+  function onThumbnailClick(key: string) {
+    setImageIndex(
+      thumbnails.findIndex((element) => {
+        return element === key;
+      })
+    );
+  }
+
   //render row of images
   const ImageRow: JSX.Element = (
-    <div id="image-row" className="flex flex-row">
-      {images.map((image) => {
-        return <img key={image} src={image} />;
+    <div id="image-row" className="flex flex-row justify-between pt-10">
+      {thumbnails.map((thumbnail, index) => {
+        let conditionalStyle: string = "";
+        let border: string = "";
+        if (index === imageIndex) {
+          conditionalStyle = "opacity-40";
+          border = "border-2 rounded-lg border-solid border-customOrange";
+        }
+        return (
+          <div className={border}>
+            <img
+              onClick={() => {
+                onThumbnailClick(thumbnail);
+              }}
+              key={thumbnail}
+              src={thumbnail}
+              className={`max-w-[88px] hover:opacity-40 aspect-auto ${conditionalStyle} rounded-lg`}
+            />
+          </div>
+        );
       })}
     </div>
   );
 
   return (
-    <div className="flex flex-row items-center w-full relative">
-      <button
-        className="flex items-center justify-center absolute left-5 bg-white w-10 h-10 rounded-full rotate-180 shrink-0"
-        onClick={onMinus}
+    <div id="image-container" className="md:w-[445px]">
+      <div
+        id="image"
+        className="flex flex-row items-center relative md:w-[455px]"
       >
-        <img src={IconNext} />
-      </button>
+        {isMobile && (
+          <ArrowButton src={IconNext} onClick={onMinus} direction="left" />
+        )}
 
-      <img className="aspect-[5/4] 	 w-full" src={images[imageIndex]} />
+        <img
+          className="aspect-[5/4] md:w-[445px] md:h-[445px] md:aspect-square md:rounded-lg w-full"
+          src={images[imageIndex]}
+        />
 
-      <button
-        className="flex items-center justify-center absolute right-5 bg-white w-10 h-10 rounded-full shrink-0"
-        onClick={onPlus}
-      >
-        <img src={IconNext} />
-      </button>
-
-      {/* {TODO: conditionally render ImageRow} */}
+        {isMobile && (
+          <ArrowButton src={IconNext} onClick={onPlus} direction="right" />
+        )}
+      </div>
+      {!isMobile && ImageRow}
     </div>
   );
 }
